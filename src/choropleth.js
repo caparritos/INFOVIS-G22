b51f4ed6ccd199f244788801cec5e76a4967037d
Daniel Pereira <72202548+DaniPalma2002@users.noreply.github.com>
2024-10-23 02:16:46 +0100

    make map not draw every time (legend broken)

--- src/choropleth.js
+++ src/choropleth.js
@@ -6,8 +6,6 @@ var previousCountry = null;
 
 let geoData, disasterData;
 
-let g, colorScale;
-
 function updateChoropleth(minYear, maxYear, country, globalFilter) {
   selectedCountry = country;
   previousCountry = country;
@@ -89,31 +87,7 @@ function drawMap(minYear, maxYear, country, globalFilter) {
   const missingDataColor = "#d9d9d9";
   const selectedCountryColor = "#1ed928";
 
-  // let path;
-  // if (!mapDrawn) {
-    d3.select("#choropleth").select("svg").remove();
-    const svg = d3
-      .select("#choropleth")
-      .append("svg")
-      .attr("width", 730)
-      .attr("height", 450);
-
-    const width = 830;
-    const height = 600;
-
-    // Mapa e projeção
-    const projection = d3
-      .geoMercator()
-      .scale(100)
-      .center([0, 20])
-      .translate([width / 2, height / 2]);
-
-    g = svg.append("g");
-     
-    path = d3.geoPath().projection(projection);
-  // }
-
-  colorScale =
+  let colorScale =
     globalFilter === "num_disasters"
       ? d3
           .scaleThreshold()
@@ -142,6 +116,101 @@ function drawMap(minYear, maxYear, country, globalFilter) {
             "#4a016a",
             "#2f004f", // Darkest red
           ]);
+
+  if (!mapDrawn) {
+    d3.select("#choropleth").select("svg").remove();
+    const svg = d3
+      .select("#choropleth")
+      .append("svg")
+      .attr("width", 730)
+      .attr("height", 450);
+
+    const width = 830;
+    const height = 600;
+
+    // Mapa e projeção
+    const projection = d3
+      .geoMercator()
+      .scale(100)
+      .center([0, 20])
+      .translate([width / 2, height / 2]);
+
+    g = svg.append("g");
+
+    path = d3.geoPath().projection(projection);
+
+    // Criar um grupo para a escala
+    const legendGroup = svg.append("g").attr("transform", "translate(30, 40)"); // Ajuste a posição conforme necessário
+
+    const legendHeight = 300;
+    const legendWidth = 20;
+
+    // Desenhar retângulos coloridos para a escala
+    colorScale.range().forEach((color, i) => {
+      legendGroup
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", (legendHeight / colorScale.range().length) * i) // Distribui verticalmente
+        .attr("width", legendWidth)
+        .attr("height", legendHeight / colorScale.range().length) // Altura proporcional à barra
+        .attr("fill", color);
+    });
+
+    // Add lines separating each square of the color scale
+    colorScale.range().forEach((_, i) => {
+      // Skip the first square
+      legendGroup
+        .append("line")
+        .attr("x1", 0)
+        .attr("y1", (legendHeight / colorScale.range().length) * (i + 1))
+        .attr("x2", legendWidth + 5)
+        .attr("y2", (legendHeight / colorScale.range().length) * (i + 1))
+        .attr("stroke", "#000") // Line color
+        .attr("stroke-width", 1); // Line width
+    });
+
+    // Adicionar rótulos à escala
+    const ticks = colorScale.domain(); // Valores para os rótulos
+    ticks.forEach((tick, i) => {
+      // Adiciona rótulos somente se não for o último
+      if (i < ticks.length) {
+        legendGroup
+          .append("text")
+          .attr("x", legendWidth + 10) // Posição do rótulo à direita dos retângulos
+          .attr("y", (legendHeight / colorScale.range().length) * (i + 1)) // Centraliza verticalmente
+          .attr("dy", ".35em") // Alinha verticalmente ao centro
+          .attr("font-size", "13px")
+          .text(tick < 500 ? tick : d3.format(".1~s")(tick)); // Formatação dos rótulos
+      }
+    });
+
+    // Add label
+    legendGroup
+      .append("text")
+      .attr("x", legendWidth - 20)
+      .attr("y", -10) // Primeiro rótulo na parte superior
+      .attr("font-size", "14px")
+      .text(globalFilter == "num_disasters" ? "Disasters" : "Deaths"); // O menor valor
+
+    // Adicionar rótulo do valor máximo
+    // legendGroup
+    //   .append("text")
+    //   .attr("x", legendWidth + 5)
+    //   .attr("y", legendHeight - 5) // Último rótulo na parte inferior
+    //   .text(d3.max(ticks)); // O maior valor no domínio
+
+    // Add zoom functionality
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 8]) // Define a escala mínima e máxima
+      .on("zoom", (event) => {
+        g.attr("transform", event.transform); // Aplica a transformação de zoom ao grupo
+      });
+
+    svg.call(zoom); // Aplica o zoom ao SVG
+  }
+
+  
 
   // Criação de um grupo para aplicar o zoom
 
@@ -284,82 +353,11 @@ function drawMap(minYear, maxYear, country, globalFilter) {
           updateRadialChart(minYear, maxYear, selectedCountry, globalFilter);
           updateScatterPlot(minYear, maxYear, selectedCountry);
         });
-      // Criar um grupo para a escala
-      const legendGroup = svg
-        .append("g")
-        .attr("transform", "translate(30, 40)"); // Ajuste a posição conforme necessário
-
-      const legendHeight = 300;
-      const legendWidth = 20;
-
-      // Desenhar retângulos coloridos para a escala
-      colorScale.range().forEach((color, i) => {
-        legendGroup
-          .append("rect")
-          .attr("x", 0)
-          .attr("y", (legendHeight / colorScale.range().length) * i) // Distribui verticalmente
-          .attr("width", legendWidth)
-          .attr("height", legendHeight / colorScale.range().length) // Altura proporcional à barra
-          .attr("fill", color);
-      });
-
-      // Add lines separating each square of the color scale
-      colorScale.range().forEach((_, i) => {
-        // Skip the first square
-        legendGroup
-          .append("line")
-          .attr("x1", 0)
-          .attr("y1", (legendHeight / colorScale.range().length) * (i + 1))
-          .attr("x2", legendWidth + 5)
-          .attr("y2", (legendHeight / colorScale.range().length) * (i + 1))
-          .attr("stroke", "#000") // Line color
-          .attr("stroke-width", 1); // Line width
-      });
-
-      // Adicionar rótulos à escala
-      const ticks = colorScale.domain(); // Valores para os rótulos
-      ticks.forEach((tick, i) => {
-        // Adiciona rótulos somente se não for o último
-        if (i < ticks.length) {
-          legendGroup
-            .append("text")
-            .attr("x", legendWidth + 10) // Posição do rótulo à direita dos retângulos
-            .attr("y", (legendHeight / colorScale.range().length) * (i + 1)) // Centraliza verticalmente
-            .attr("dy", ".35em") // Alinha verticalmente ao centro
-            .attr("font-size", "13px")
-            .text(tick < 500 ? tick : d3.format(".1~s")(tick)); // Formatação dos rótulos
-        }
-      });
-
-      // Add label
-      legendGroup
-        .append("text")
-        .attr("x", legendWidth - 20)
-        .attr("y", -10) // Primeiro rótulo na parte superior
-        .attr("font-size", "14px")
-        .text(globalFilter == "num_disasters" ? "Disasters" : "Deaths"); // O menor valor
-
-      // Adicionar rótulo do valor máximo
-      // legendGroup
-      //   .append("text")
-      //   .attr("x", legendWidth + 5)
-      //   .attr("y", legendHeight - 5) // Último rótulo na parte inferior
-      //   .text(d3.max(ticks)); // O maior valor no domínio
-
-      // Add zoom functionality
-      const zoom = d3
-        .zoom()
-        .scaleExtent([1, 8]) // Define a escala mínima e máxima
-        .on("zoom", (event) => {
-          g.attr("transform", event.transform); // Aplica a transformação de zoom ao grupo
-        });
-
-      svg.call(zoom); // Aplica o zoom ao SVG
     })
     .catch((err) => {
       console.error("Error loading the data: ", err);
     });
-    mapDrawn = true;
+  mapDrawn = true;
 }
 // Executa o drawMap quando o DOM estiver totalmente carregado
 document.addEventListener("DOMContentLoaded", function () {
